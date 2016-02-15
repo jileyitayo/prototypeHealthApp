806d690a65bc4361f9618e505acb1d03583fafe4
JIL <jileyitayo@gmail.com>
2016-02-16 00:10:09 +0100

    v1.01
    New Update v1.01

---
+++ app/src/main/java/com/example/jil/myproject_prototype2sqlite/fragment_three.java
@@ -0,0 +1,24 @@
+package com.example.jil.myproject_prototype2sqlite;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class fragment_three extends Fragment {
+    public fragment_three()
+    {
+
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_fragment_three,container,false);
+    }
+}
--- app/src/main/java/com/example/jil/myproject_prototype2sqlite/toolbar10.java
+++ app/src/main/java/com/example/jil/myproject_prototype2sqlite/toolbar10.java
@@ -1,15 +1,28 @@
 package com.example.jil.myproject_prototype2sqlite;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.CompoundButton;
-import android.widget.Switch;
-import android.widget.ToggleButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.example.jil.myproject_prototype2sqlite.fragment_one;
+import com.example.jil.myproject_prototype2sqlite.fragment_two;
+import com.example.jil.myproject_prototype2sqlite.fragment_three;
+import com.example.jil.myproject_prototype2sqlite.fragment_four;
+import com.example.jil.myproject_prototype2sqlite.fragment_five;
 public class toolbar10 extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +31,14 @@ public class toolbar10 extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
 
     }
 
@@ -31,5 +52,50 @@ public class toolbar10 extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+    //my custom methods
+
+    //this setups the view pager
+    public void setupViewPager(ViewPager viewPager)
+    {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(new fragment_one(), "ONE");
+        adapter.addFragment(new fragment_two(), "TWO");
+        adapter.addFragment(new fragment_three(), "THREE");
+        viewPager.setAdapter(adapter);
+    }
+
+
+    ///custom Classes
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
